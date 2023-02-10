040e10257907f9042caaa49321cca0d4e9e20335
Andrey Kucherenko <andykucherenko@gmail.com>
2023-02-10 10:39:59 +0300

    Task solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int account = 100;
+        int accountRefill = 1100;
+        int bonus;
+
+        if (accountRefill > 1000) {
+            bonus = accountRefill / 100;
+        } else {
+            bonus = 0;
+        }
+
+        int balance = account + accountRefill + bonus;
+        System.out.println("Спасибо за ваш платеж. Ваш баланс: " + balance + " руб. Ваш бонус: " + bonus + " руб.");
     }
 }
